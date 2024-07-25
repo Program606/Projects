28b6b3282c4e7acfbcf6e31caae02c8ffe6257cd
Prince Manzano <Pmanz282@mtroyal.ca>
2024-07-25 12:44:52 -0600

    identical to last commit

--- Maven Finance Project/financeproject/src/main/java/com/prince/controller/App.java
+++ Maven Finance Project/financeproject/src/main/java/com/prince/controller/App.java
@@ -3,11 +3,13 @@ package com.prince.controller;
 import java.io.IOException;
 
 import com.prince.application.FinanceApplication;
+import com.prince.excelHandling.ReadExcelFileDemo;
 public class App
 {
     public static void main( String[] args ) throws IOException
     {
-        FinanceApplication fp = new FinanceApplication();
-		fp.run();
+        // FinanceApplication fp = new FinanceApplication();
+		// fp.run();
+        ReadExcelFileDemo.test();
     }
 }
